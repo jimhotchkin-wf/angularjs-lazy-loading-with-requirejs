100bf5fb49f354f163772b14a1efd9d00b78bd29
Jim.Hotchkin <jim.hotchkin@webfilings.com>
2013-05-15 09:37:28 -0500

    adding adding grunt build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,17 +20,43 @@ module.exports = function(grunt) {
             }
         },
         useminPrepare: {
-            html: 'app/index.html'
+            html: 'dist/index.html'
         },
         usemin: {
-            html: ['app/index.html']
+            html: ['dist/index.html']
+        },
+        copy: {
+          main: {
+            files: [
+                {expand: true, cwd: 'app/', src: '*.html', dest: 'dist/'},
+                {expand: true, cwd: 'app/', src: 'scripts/**', dest: 'dist/'},
+                {expand: true, cwd: 'app/', src: 'styles/**', dest: 'dist/'},
+                {expand: true, cwd: 'app/', src: 'vendor/**', dest: 'dist/'},
+                {expand: true, cwd: 'app/', src: 'views/**', dest: 'dist/'}
+            ]
+          }
         }
+        // copy: {
+        //   main: {
+        //     files: [
+        //       {expand: true, cwd: 'dist/', src: ['app/*.*'], dest: 'index.html', filter: 'isFile'}, //copies only files
+        //       {expand: true, cwd: 'dist/', src: ['app/scripts/**'], dest: 'scripts/'}, // includes files in path and its subdirs
+        //       {expand: true, cwd: 'dist/', src: ['app/styles/**'], dest: 'styles/'}, // includes files in path and its subdirs
+        //       {expand: true, cwd: 'dist/', src: ['app/vendor/**'], dest: 'vendor/'}, // includes files in path and its subdirs
+        //       {expand: true, cwd: 'dist/', src: ['app/views/**'], dest: 'views/'} // includes files in path and its subdirs
+        //       // {expand: true, cwd: 'path/', src: ['**'], dest: 'dest/'}, // makes all src relative to cwd
+        //       // {expand: true, flatten: true, src: ['path/**'], dest: 'dest/', filter: 'isFile'} // flattens results to a single level
+        //       // {src: ['css/**', 'img/**', 'js/**', 'release/**', 'lib/**', 'locale/**', '!**/*.less'], dest: 'output/toolkit/'}
+        //     ]
+        //   }
+        // }
     });
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-compass');
     grunt.loadNpmTasks('grunt-usemin');
-    grunt.registerTask('build', ['useminPrepare', 'concat', 'uglify', 'usemin']);
+    grunt.registerTask('build', ['compass', 'copy', 'useminPrepare', 'concat', 'uglify', 'usemin']);
     grunt.registerTask('default', ['compass']);
 };
